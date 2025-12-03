09114ba6a4a67ddb867eb05df2ee930b0926638d
ArdaOzcanCifci <250542012@firat.edu.tr>
2025-12-03 10:41:31 +0300

    Add comments on compiler errors in Exercise4_4
    Added comments explaining compiler errors for return value and void method usage.

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,10 +11,13 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
+        // cevap : int degerinde veri dondurmedigi icin compier hata veriyor.
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        // cevap : operator void ve int degerlerini toplayamiyor ve compiler hata veriyor.
+
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
