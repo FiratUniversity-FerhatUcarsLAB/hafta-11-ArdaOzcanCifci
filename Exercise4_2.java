9cd04d18cb1edf10f8890193726a7030827db67d
ArdaOzcanCifci <250542012@firat.edu.tr>
2025-12-03 11:29:00 +0300

    Exercise4_2 tamamlandı

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -3,21 +3,29 @@ public class Exercise4_2 {
   // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
     public static void main(String[] args) {
-        zippo("rattle", 13);
+        zippo("rattle", 13); // rattle kelimesi baffle metoduna gonderilen blimp parametresinin degeridir 
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+        System.out.println(blimp); // 2
+        zippo("ping", -5); // 3
     }
 
     public static void zippo(String quince, int flag) {
         if (flag < 0) {
-            System.out.println(quince + " zoop");
+            System.out.println(quince + " zoop"); // 4
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik"); // 1
+            baffle(quince); // motota gitti
+            System.out.println("boo-wa-ha-ha"); // 5
         }
     }
 }
+/*
+    Cikti :
+    ik
+    rattle
+    ping zoop
+    boo-wa-ha-ha
+
+*/
